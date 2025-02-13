74e9e02054c85ba48b4ae82b425136b586527798
Feanor806 <91660671+Feanor806@users.noreply.github.com>
2025-02-13 18:48:38 +0300

    Классная работа 13022025

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите число A:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число Б:");
+            byte b = Convert.ToByte(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine($"Сумма {a} + {b} = {sum}");
+            Console.WriteLine("Нажмите любую кнопку");
+            Console.ReadKey();
         }
     }
 }
