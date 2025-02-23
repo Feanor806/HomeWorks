b9236d929d84b35c78174688fdfb8c3066ca4e91
Feanor806 <91660671+Feanor806@users.noreply.github.com>
2025-02-23 21:27:14 +0300

    Выполнение блока заданий 3

---
+++ Task3_10_CountHouses/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task3_10_CountHouses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Введение числа
+            Console.Write("Введите число построенных домов: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            if (count % 10 == 1 && count % 100 != 11)
+            {
+                Console.WriteLine($"Был построен {count} дом.");
+            }
+            else if ((count % 10 == 2 && count % 100 != 12) || (count % 10 == 3 && count % 100 != 13) || (count % 10 == 4 && count % 100 != 14))
+            {
+                Console.WriteLine($"Было построено {count} дома.");
+            }
+            else 
+            {
+                Console.WriteLine($"Было построено {count} домов.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3_4_Medium/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task3_4_Medium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Введение трех чисел
+            Console.Write("Введите число A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число C: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            //Сравнение числел и вывод результата
+
+            if (a==b && a==c)
+            {
+                Console.WriteLine($"Все числа равны, среднее значение: {a}");
+            }
+            else if ((a>=b && a<=c) || (a >= c && a <= b))
+            {
+                Console.WriteLine($"Среднее значение имеет число A: {a}");
+            }
+            else if ((b >= a && b <= c) || (b >= c && b <= a))
+            {
+                Console.WriteLine($"Среднее значение имеет число B: {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Среднее значение имеет число С: {c}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
