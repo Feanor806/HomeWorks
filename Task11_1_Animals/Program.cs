74b624bf2eec3c5bc975e8a365e1cdf1f898ef97
Михаил Солнцев <mihsoln@live.com>
2025-04-20 15:35:34 +0300

    Выполнение блока 11

---
+++ Task11_1_Animals/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task11_1_Animals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals =
+            [
+                new Dog(),
+                new Cat()
+            ];
+
+            foreach (var animal in animals)
+            {
+                animal.GetInfo();
+            }
+            Console.ReadKey();
+        }
+
+        public abstract class Animal
+        {
+            public abstract string Name { get;}
+            public abstract void Say();
+            public void GetInfo()
+            {
+                Console.WriteLine($"{Name}");
+                Say();
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public override string Name => "Собака";
+            public override void Say()
+            {
+                Console.WriteLine($"Гав!");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public override string Name => "Кошка";
+            public override void Say()
+            {
+                Console.WriteLine($"Мяу!");
+            }
+        }
+    }
+}
---
+++ Task11_2_Flight/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Task11_2_Flight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable[] flights = [
+                    new Bird(4500),
+                    new Airpalane(10000,350)
+                ];
+
+            foreach (IFlyable fly in flights)
+            {
+                fly.Fly();
+            }
+
+            Console.ReadKey();
+        }
+
+        public interface IFlyable
+        {
+            public int MaxAltitude { get; }
+            public void Fly();
+        }
+
+        public class Bird : IFlyable
+        {
+
+            public int MaxAltitude { get; }
+
+            public void Fly()
+            {
+
+                Console.WriteLine($"Я лечу на высоте {MaxAltitude} метров");
+            }
+
+            public Bird(int maxAltitude)
+            {
+                MaxAltitude = maxAltitude;
+            }
+        }
+
+        public class Airpalane : IFlyable
+        {
+            public int MaxAltitude { get; }
+
+            public int CountPassengers { get; set; }
+
+            public void Fly()
+            {
+                Console.WriteLine($"Я лечу на высоте {MaxAltitude} метров, везу {CountPassengers} пассажиров");
+            }
+
+            public Airpalane(int maxAltitude, int countPassengers)
+            {
+                MaxAltitude = maxAltitude;
+                CountPassengers = countPassengers;
+            }
+        }
+    }
+}
