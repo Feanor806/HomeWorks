5c11684ea2f9262ae664f28da158d707742f7ef0
Feanor806 <91660671+Feanor806@users.noreply.github.com>
2025-05-15 21:53:28 +0300

    Выполнение блока 13

---
+++ Task13_3_RPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task13_3_RPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
